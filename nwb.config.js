d9a90665a74ae745bbf72969bd3853eeaf113b63
Lane Olson <laneolson@gmail.com>
2019-09-25 11:15:18 -0600

    fix: add babel config for ie11

--- nwb.config.js
+++ nwb.config.js
@@ -9,6 +9,22 @@ module.exports = {
       }
     }
   },
+  babel: {
+    presets: [
+      ["env", {
+        "targets": {
+          "browsers": [
+            "Chrome >= 52",
+            "FireFox >= 44",
+            "Safari >= 7",
+            "Explorer 11",
+            "last 4 Edge versions"
+          ]
+        }
+      }],
+      "es2015", "react", "stage-0"
+    ],
+  },
   webpack: {
     html: {
       template: 'demo/src/index.html'
